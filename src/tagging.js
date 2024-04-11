16cf345aed5808c48c1e3b0aa575d7c36235cba6
MarissaTolomeo <156345123+MarissaTolomeo@users.noreply.github.com>
2024-04-11 12:46:53 -0400

    setting up document in class 04/11

---
+++ src/tagging.js
@@ -0,0 +1,35 @@
+import { LitElement, html, css } from 'lit';
+
+
+export class tagging extends LitElement {
+
+  static get tag() {
+    return 'tagging';
+  }
+
+  constructor() {
+    super();
+ 
+  }
+
+  static get styles() {
+    return css`
+     
+      
+    `;
+  }
+
+  render() {
+    return html`
+
+    `
+ }
+
+  static get properties() {
+    return {
+   
+    };
+  }
+}
+
+globalThis.customElements.define(tagging.tag, tagging);
